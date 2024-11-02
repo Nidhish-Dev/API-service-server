b7e819f39b0db7ea55deeaaa7f298cc0ff496a11
Nidhish Rathore <codenidhish07@gmail.com>
2024-11-02 19:19:09 +0530

    api verification fix

--- middleware/verifyApiKey.js
+++ middleware/verifyApiKey.js
@@ -1,25 +1,26 @@
-const User = require('../models/User');
+const User = require('../models/User'); 
 
 exports.verifyApiKey = async (req, res, next) => {
     const apiKey = req.query.apiKey; 
+    console.log('Received API key:', apiKey); 
 
     if (!apiKey) {
         return res.status(403).json({ error: 'API key is required' });
     }
 
     try {
-
         const user = await User.findOne({ 'apiKeys.key': apiKey });
         
+        console.log('User found:', user); 
+
         if (!user) {
             return res.status(403).json({ error: 'Invalid API key' });
         }
 
         req.user = user; 
-
         next();
     } catch (error) {
-        console.error('Error verifying API key:', error);
+        console.error('Error verifying API key:', error); 
         res.status(500).json({ error: 'Error verifying API key' });
     }
 };
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,6 @@ const { verifyApiKey } = require('../middleware/verifyApiKey');
 const router = express.Router();
 const {getHello} = require('../controllers/apiController')
 
-router.get('/hello', getHello);
+router.get('/hello',verifyApiKey, getHello);
 
 module.exports = router;
